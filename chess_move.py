12bdcf8c905bbefc4c97e20d294f48e2b2201940
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2022-01-22 22:00:40 +0100

    Added docstrings in the chess_move file.

--- chess_move.py
+++ chess_move.py
@@ -3,6 +3,29 @@ from two_player_games.two_player_games.move import Move
 
 
 class ChessMove(Move):
+    """
+    A class representing moves in chess.
+
+
+    Attributes:
+
+    _start_column : int
+        an integer representing the column of a square
+        from which the move originates
+
+    _start_row : int
+        an integer representing the row of a square
+        from which the move originates
+
+    _end_column : int
+        an integer representing the column of a square
+        that is the destination of the move
+
+    _end_row : int
+        an integer representing the row of a square
+        that is the destination of the move
+    """
+
     def __init__(
         self,
         start_column: int,
@@ -10,6 +33,31 @@ class ChessMove(Move):
         end_column: int,
         end_row: int,
     ):
+
+        """
+        Constructor of the ChessMove class. Function checks if the given
+        arguments are within the range of available coordinates.
+
+
+        Parameters:
+
+        start_column : int
+            an integer representing the column of a square
+            from which the move originates
+
+        start_row : int
+            an integer representing the row of a square
+            from which the move originates
+
+        end_column : int
+            an integer representing the column of a square
+            that is the destination of the move
+
+        end_row : int
+            an integer representing the row of a square
+            that is the destination of the move
+        """
+
         if all(
             coordinate in range(8)
             for coordinate in (start_column, start_row, end_column, end_row)
@@ -22,18 +70,23 @@ class ChessMove(Move):
             raise CoordinatesOutOfBoundsException
 
     def start_column(self) -> int:
+        """Returns the column of an originating square."""
         return self._start_column
 
     def start_row(self) -> int:
+        """Returns the row of an originating square."""
         return self._start_row
 
     def end_column(self) -> int:
+        """Returns the column of a destination square."""
         return self._end_column
 
     def end_row(self) -> int:
+        """Returns the row of a destination square."""
         return self._end_row
 
     def __eq__(self, other: object) -> bool:
+        """Checks if two moves are equal."""
         if not isinstance(other, ChessMove):
             return False
         return (
@@ -44,5 +97,8 @@ class ChessMove(Move):
         )
 
     def __repr__(self) -> str:
+        """
+        Return object info for debugging (originating and destination square).
+        """
         return f"{chr(self._start_column + ord('a'))}{self._start_row + 1} move to \
 {chr(self._end_column + ord('a'))}{self._end_row + 1}"
