c960c1277b29d4b45a94a09fbb26c92d8af1c349
Chr1sps <91991025+Chr1sps@users.noreply.github.com>
2022-01-23 03:09:46 +0100

    Added docstrings in the chess_game file. Rewritten the ChessGame constructor to not create _first_player and _second_player attributes. Changed a type hint in the make_move method in the ChessPiece class.

--- chess_game.py
+++ chess_game.py
@@ -5,7 +5,18 @@ from chess_state import ChessState
 
 
 class ChessGame(Game):
-    """Class that represents a chess game"""
+    """
+    A class that represents a chess game.
+
+
+    Attributes:
+
+    state : ChessState
+        a ChessState object that represents the current state of the board
+        (which player is playing the move, which player is waiting for their
+        move, which player is playing white, what pieces are on the board,
+        etc.)
+    """
 
     FIRST_PLAYER_DEFAULT_CHAR = "W"
     SECOND_PLAYER_DEFAULT_CHAR = "B"
@@ -13,20 +24,53 @@ class ChessGame(Game):
     def __init__(
         self, first_player: Player = None, second_player: Player = None
     ):
+        """
+        ChessGame class constructor.
 
-        self._first_player = first_player or Player(
-            self.FIRST_PLAYER_DEFAULT_CHAR
-        )
-        self._second_player = second_player or Player(
+
+        Parameters:
+
+        first_player : Player
+            a Player object that represents the player moving first in the game
+            (and, therefore, playing white)
+
+        second_player : Player
+            a Player object representing the player moving second in the game
+            (and, therefore, playing black)
+        """
+        _first_player = first_player or Player(self.FIRST_PLAYER_DEFAULT_CHAR)
+        _second_player = second_player or Player(
             self.SECOND_PLAYER_DEFAULT_CHAR
         )
 
-        state = ChessState(self._first_player, self._second_player)
+        state = ChessState(_first_player, _second_player)
 
         super().__init__(state)
 
     def is_promotion(self, move: ChessMove) -> bool:
+        """
+        Returns True if a move will result in a promotion.
+
+
+        Parameters:
+
+        move : ChessMove
+            a ChessMove object representing a move
+        """
         return self.state.is_promotion(move)
 
-    def make_move(self, move: ChessMove, promotion_type: int = None):
+    def make_move(self, move: ChessMove, promotion_type: type = None):
+        """
+        Makes a move and updates the state attribute accordingly.
+
+
+        Parameters:
+
+        move : ChessMove
+            a ChessMove object representing the move being made
+
+        promotion_type : type
+            a type that represents the class of a piece to which the pawn will
+            promote
+        """
         self.state = self.state.make_move(move, promotion_type)
